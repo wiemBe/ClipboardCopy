7055cd4347d027be490cebea430713ac5714e394
efe <bayefeuzunoglu@gmail.com>
2024-05-02 15:11:21 +0300

    added some time func

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import win32clipboard
+import schedule
+import time 
 
-win32clipboard.OpenClipboard()
-clipboardData = win32clipboard.GetClipboardData(win32clipboard.CF_TEXT)
-win32clipboard.CloseClipboard()
+def job():
 
-print("twxt from clipboard", clipboardData.decode("utf-8"))
+    win32clipboard.OpenClipboard()
+    clipboardData = win32clipboard.GetClipboardData(win32clipboard.CF_TEXT)
+    win32clipboard.CloseClipboard()
+
+    print("Text from clipboard:", clipboardData.decode("utf-8"))
+
+schedule.every(10).seconds.do(job)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
